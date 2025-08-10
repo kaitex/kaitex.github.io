e19a0a93b49f42e631c6fd3ffc0c546117c38d98
bidurmajgianya <majgianyabidur02@gmail.com>
2025-08-10 20:55:30 +0545

    nothing changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/ThemeContext";
-import GoatCounterTracker from "@/components/GoatCountTracker";
+
 
 
 const geistSans = Geist({
@@ -29,19 +29,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
-        {process.env.NODE_ENV === "production" && (
-          <script
-            data-goatcounter="https://kaitex.goatcounter.com/count"
-            async
-            src="//gc.zgo.at/count.js"
-          />
-        )}
+        
       </head>
       <body className="max-w-5xl py-5 mx-auto px-5 sm:px-20">
         <ThemeProvider>
           <Navbar />
           <main>{children}</main>
-          {process.env.NODE_ENV === "production" && <GoatCounterTracker />}
+      
         </ThemeProvider>
       </body>
     </html>
