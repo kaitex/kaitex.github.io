9c36218b2b080f558532824941f7b002d529c363
kaitex <majgianyabidur02@gmail.com>
2025-12-31 20:01:45 +0545

    just main content is chaged

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 export default function Hero() {
   return (
-    <section className="mt-5 md:mt-15 min-h-[70vh] flex items-center">
+    <section className="mt-5 md:mt-15 min-h-[70vh]">
+
       <div className="flex flex-col max-w-2xl">
 
         <div className="w-20 h-20 rounded-full overflow-hidden">
@@ -18,12 +19,7 @@ export default function Hero() {
 
 
         <p className="mt-5 text-[var(--text-color)] opacity-80 text-md md:text-lg leading-relaxed">
-          Senior Full Stack Engineer specializing in React, Next.js, Node.js, and .Net .
-        </p>
-        <p className="mt-5 text-[var(--text-color)] opacity-80 text-md md:text-lg leading-relaxed">
-         I build end-to-end applications — from scalable web platforms to APIs that power real-time business solutions.
-        </p>
-
+          Junior Software Engineer.</p>
         <div className="flex flex-wrap gap-4 mt-5 text-sm md:text-base">
           <a
             href="https://www.linkedin.com/in/bidur-majgianya-196318248/"
@@ -31,7 +27,7 @@ export default function Hero() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            LinkedIn 
+            LinkedIn
           </a>
           <a
             href="https://github.com/kaitex"
@@ -39,19 +35,19 @@ export default function Hero() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            GitHub 
+            GitHub
           </a>
           <a
             href="mailto:majgianyabidur02@gmail.com"
             className="text-[var(--text-color)] opacity-70 hover:opacity-100 transition-opacity"
           >
-            Email 
+            Email
           </a>
           <a
             href="/posts"
             className="text-[var(--text-color)] opacity-70 hover:opacity-100 transition-opacity"
           >
-            Blog 
+            Blog
           </a>
         </div>
       </div>
